3cc28c2da292ca47d6045b9d24c1503565a8472b
yingzya <yingzya@users.noreply.github.com>
2025-08-21 01:14:10 +0000

    deploy: dcefdd0ef7e621e45a63f137197889887395dfee

---
+++ _nuxt/BbWYdUpL.js
@@ -0,0 +1,1 @@
+import{s as o,k as s,o as r,G as t}from"./rwJxNEL4.js";const n={};function a(e,c){return r(),s("tbody",null,[t(e.$slots,"default")])}const _=Object.assign(o(n,[["render",a]]),{__name:"ProseTbody"});export{_ as default};
